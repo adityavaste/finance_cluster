79f345d3930b08045c8e8813e6408fcb9e2a9eb5
adityavaste <adityavaste@gmail.com>
2026-06-13 13:44:52 +0530

    correction in main.tf line 1

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source = "./modules/vpc"
-
+k_cluster_name = var.k_cluster_name
   vpc_cidr            = var.vpc_cidr
   public_subnet_cidr  = var.public_subnet_cidr
   private_subnet_cidr = var.private_subnet_cidr
